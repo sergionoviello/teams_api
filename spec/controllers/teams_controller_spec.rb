5873c07778c9c5d5e59d444fa4eedde67ce4c825
Sergio Noviello <sergio.noviello@storyful.com>
2017-08-19 23:42:10 +0100

    controller tests

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -1,5 +1,37 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe TeamsController, type: :controller do
+RSpec.describe Api::V1::TeamsController, type: :request do
+  describe 'POST create teams' do
+    let(:user) { create(:user, email: 'example@mail.com', password: '123123123') }
+    let(:token) { AuthenticateUser.new(user.email, user.password).call }
 
-# end
+    before do
+      body = {
+        data: {
+          type: 'teams',
+          attributes: {
+            name: 'Pegasus',
+            description: 'delivery team'
+          }
+        }
+      }
+
+      post '/api/v1/teams',
+           params: body.to_json,
+           headers: {
+             'Accept' => 'application/vnd.api+json',
+             'Content-type' => 'application/vnd.api+json',
+             'Authorization' => token
+           }
+    end
+
+    it { expect(response).to have_http_status(:success) }
+    it { expect(json['data']['type']).to eq('teams') }
+
+    context 'when is not authorized' do
+      let(:token) { '' }
+
+      it { expect(response).to have_http_status(:unauthorized) }
+    end
+  end
+end
