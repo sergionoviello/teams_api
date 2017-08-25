a2589a1e9ea5f8eace15e92132218453f3dedd4e
Sergio Noviello <sergio.noviello@storyful.com>
2017-08-25 10:07:49 +0100

    adding dependent callback to team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 # Taam
 class Team < ApplicationRecord
-  has_many :employees
-  has_many :projects
+  has_many :employees, dependent: :restrict_with_error
+  has_many :projects, dependent: :restrict_with_error
 
   validates :name, presence: true, uniqueness: true
 end
